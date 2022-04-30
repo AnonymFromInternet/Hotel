5d94f8ab2afcd4a5a2573d64f518cdc33cb5a473
AnonymFromInternet <84092079+AnonymFromInternet@users.noreply.github.com>
2022-04-30 23:12:36 +0200

    Commit

--- internal/repository/repository.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-import "github.com/anonymfrominternet/Hotel/internal/models"
-
-type DatabaseRepo interface {
-	AllUsers() bool
-
-	InsertReservation(reservation models.Reservation) error
-}
