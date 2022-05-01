aaa86534e4a574bf9721f7bc800ab726522dbcd7
AnonymFromInternet <84092079+AnonymFromInternet@users.noreply.github.com>
2022-05-01 18:37:48 +0200

    Adding Packages

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
-
-func MainPage(writer http.ResponseWriter, request *http.Request) {
-	_, _ = fmt.Fprintf(writer, "This is main page")
-}
-func AboutPage(writer http.ResponseWriter, request *http.Request) {
-	_, _ = fmt.Fprintf(writer, "This is about page")
-}
 
 func main() {
 	http.HandleFunc("/", MainPage)
 	http.HandleFunc("/about", AboutPage)
+
 	_ = http.ListenAndServe(":3000", nil)
 }
--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@ This is the repository for Hotel site
 - Uses the [chi router](https://github.com/go-chi/chi/)
 - Uses [SCS](https://github.com/alexedwards/scs/) session management
 - Uses [nosurf](https://github.com/justinas/nosurf)
+- Uses [Bootstrap](https://getbootstrap.com/)
